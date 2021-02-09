74591f6a08f5b441dee9d90d62e06fe66958cbcd
Jeremy Wall <jeremywall@users.noreply.github.com>
2021-02-08 20:19:50 -0800

    Create Engine.java

---
+++ src/main/lombok/com/jeremywall/maven_dagger2_lombok_example/Engine.java
@@ -0,0 +1,14 @@
+package com.jeremywall.maven_dagger2_lombok_example;
+
+import lombok.NoArgsConstructor;
+
+import javax.inject.Inject;
+
+@NoArgsConstructor(onConstructor_ = @Inject)
+public class Engine {
+
+//    @Inject
+//    public Engine() {
+//
+//    }
+}
